04d7ede866d8a7d140a611ac98e568d0c0975162
Johan Petrikovsky <johan.petrikovsky@sigfox.com>
2020-09-29 10:41:29 +0200

    add sentry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,42 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import useBars from "../hooks/useBars";
+import useBars from '../hooks/useBars';
 
-import Global from "../styles/GlobalStyle";
-import { AppContainer } from "../styles/components/App.js";
+import Global from '../styles/GlobalStyle';
+import { AppContainer } from '../styles/components/App.js';
 
-import Header from "./Header";
-import Home from "./Home";
-import BarPageDetail from "./bar/BarPageDetail";
-import MePage from "./MePage";
+import Header from './Header';
+import Home from './Home';
+import BarPageDetail from './bar/BarPageDetail';
+import MePage from './MePage';
+import * as Sentry from '@sentry/browser';
+
+const RELEASE = '7e314acd80ec22e73bc706851d50ea848277dd01';
+if (process.env.NODE_ENV === 'production') {
+  console.info('production !');
+  Sentry.init({
+    dsn:
+      'https://8d6259633fb64e91aaa9b6ad76a422d7@o451965.ingest.sentry.io/5438682',
+    release: RELEASE,
+  });
+}
 
 const App = () => {
   const [bars, setBars] = useBars();
   return (
     <Router>
       <Global />
-      <AppContainer className="max-container">
+      <AppContainer className='max-container'>
         <Header />
         <Switch>
-          <Route exact path="/">
+          <Route exact path='/'>
             <Home bars={bars} />
           </Route>
-          <Route exact path="/me">
+          <Route exact path='/me'>
             <MePage />
           </Route>
-          <Route path="/bar/:id">
+          <Route path='/bar/:id'>
             <BarPageDetail bars={bars} setBars={setBars} />
           </Route>
         </Switch>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,31 @@
-import PropTypes from "prop-types";
-import React from "react";
-import BarList from "./bar/BarList";
+import PropTypes from 'prop-types';
+import React from 'react';
+import BarList from './bar/BarList';
+import * as Sentry from '@sentry/browser';
 
 const Home = ({ bars }) => {
+  const handleClick = () => {
+    try {
+      throw new Error('Caught');
+    } catch (err) {
+      if (process.env.NODE_ENV !== 'production') {
+        console.error(err);
+      }
+      Sentry.captureException(err);
+    }
+  };
+
   return (
     <div>
-      <h1 className="primary-title">Les bars de Toulouse</h1>
+      <h1 className='primary-title'>Les bars de Toulouse</h1>
+      <button onClick={handleClick}>Error</button>
       <BarList bars={bars} />
     </div>
   );
 };
 
 Home.propTypes = {
-  bars: PropTypes.array
+  bars: PropTypes.array,
 };
 
 export default Home;
